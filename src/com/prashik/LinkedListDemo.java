a1eb46d091c726ce3f18a2a6bfefa5b39f557b1b
prashikdhawane <dhawaneprashik70@gmail.com>
2023-01-08 17:32:28 +0530

    linked list changes are made

--- src/com/prashik/LinkedListDemo.java
+++ src/com/prashik/LinkedListDemo.java
@@ -13,11 +13,12 @@ public class LinkedListDemo {
 		list.add(20);
 		list.add(30);
 		list.add(40);
-		
+		/*
 		Iterator<Integer> itr=list.iterator();
 		while(itr.hasNext()) {
 			System.out.println(itr.next());
-		}
+		}*/
+		System.out.println(list);
 	}
 	
 	
