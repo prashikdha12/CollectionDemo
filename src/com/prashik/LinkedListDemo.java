96885fcd32fa14e724e16092f0585833cd498fef
prashikdhawane <dhawaneprashik70@gmail.com>
2023-01-08 17:26:37 +0530

    linked list is added

---
+++ src/com/prashik/LinkedListDemo.java
@@ -0,0 +1,25 @@
+package com.prashik;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListDemo {
+	
+	public static void main(String[] args) {
+		
+		List<Integer> list=new LinkedList<Integer>();
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		list.add(40);
+		
+		Iterator<Integer> itr=list.iterator();
+		while(itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+	}
+	
+	
+
+}
