94e10ad3338e5f188242b7a18f04f770d8f869d4
prashikdhawane <dhawaneprashik70@gmail.com>
2023-01-08 16:52:24 +0530

    collection demo

---
+++ src/com/prashik/ArrayListDemo.java
@@ -0,0 +1,18 @@
+package com.prashik;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+	
+	public static void main(String[] args) {
+		
+		List<String> list=new ArrayList<String>();
+		list.add("pune");
+		list.add("mumbai");
+		list.add("delhi");
+		list.add("bangalore");
+		System.out.println(list);
+	}
+
+}
